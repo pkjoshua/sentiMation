606efb81559c4db8f7917abb4b4def7c2275d81e
genlock <joshua9040@gmail.com>
2023-10-24 20:38:29 -0400

    sentiMation
    Test scripts created

---
+++ img2img_cn_test.py
@@ -0,0 +1,82 @@
+from PIL import Image, ExifTags
+import base64
+import requests
+import json
+
+# Function to reorient image
+def reorient_image(input_path, output_path):
+    img = Image.open(input_path)
+    
+    for orientation in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[orientation]=='Orientation':
+            break
+
+    try:
+        exif=dict(img._getexif().items())
+        if exif[orientation] == 3:
+            img=img.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            img=img.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            img=img.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        # When no orientation EXIF data
+        pass
+    
+    img.save(output_path)
+
+# Correct the orientation
+input_path = "D:\\sentimation\\image.png"
+output_path = "D:\\sentimation\\touched_image.JPG"
+reorient_image(input_path, output_path)
+
+# Read reoriented image file from disk
+with open(output_path, "rb") as image_file:
+    encoded_image = base64.b64encode(image_file.read()).decode()
+
+# Define the API URL
+api_url = "http://127.0.0.1:7860/sdapi/v1/img2img"
+
+# Define the JSON payload
+json_payload = {
+    "prompt": "cute, demon girl, sexy, revealing, detailed, portrait, cleavage",
+    "negative_prompt": "bad quality, deformed, boring",
+    "batch_size": 1,
+    "steps": 50,
+    "cfg_scale": 7,
+    "width": 512,
+    "height": 512,
+    "denoising_strength": 0.8,
+    "init_images": [encoded_image],
+    "alwayson_scripts": {
+        "controlnet": {
+            "args": [
+                {
+                    "module": "canny",
+                    "model": "control_v11p_sd15_canny_fp16 [b18e0966]"
+                }
+            ]
+        }
+    }
+}
+
+# Define headers
+headers = {
+    "Content-Type": "application/json"
+}
+
+# Make the API request
+response = requests.post(api_url, headers=headers, json=json_payload)
+
+# Parse the JSON response
+response_json = response.json()
+
+# Decode base64 image to bytes
+image_bytes = base64.b64decode(response_json['images'][0])
+
+# Define the file path
+file_path = "D:\\api_output\\img2img_cn.jpg"
+
+# Write the bytes to a file
+with open(file_path, "wb") as image_file:
+    image_file.write(image_bytes)
---
+++ txt2img_test.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import base64
+
+# Define the API URL
+api_url = "http://127.0.0.1:7860/sdapi/v1/txt2img"
+
+# Define the JSON payload
+json_payload = {
+    "prompt": "bunny, female, girl, beautiful, skimpy, revealing, detailed, portrait",
+    "negative_prompt": "bad quality, deformed, boring",
+    "seed": -1,
+    "sampler_name": "DDIM",
+    "batch_size": 1,
+    "steps": 10,
+    "cfg_scale": 7,
+    "width": 512,
+    "height": 512,
+    "denoising_strength": 0.8
+}
+
+# Convert the payload to JSON format
+json_payload_str = json.dumps(json_payload)
+
+# Define the headers
+headers = {
+    "Content-Type": "application/json"
+}
+
+# Make the API request
+response = requests.post(api_url, headers=headers, data=json_payload_str)
+
+# Parse the JSON response
+response_json = response.json()
+
+# Extract the base64 encoded image data from the 'images' field
+base64_image = response_json['images'][0]  # Assumes the first element in the 'images' array contains the base64 image data
+
+# Decode the base64 image to bytes
+image_bytes = base64.b64decode(base64_image)
+
+# Define the file path
+file_path = "D:\\api_output\\your_image.png"  # Replace 'your_image.png' with the desired file name
+
+# Write the bytes to a file
+with open(file_path, "wb") as image_file:
+    image_file.write(image_bytes)
