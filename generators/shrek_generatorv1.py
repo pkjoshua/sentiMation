723b015aa93bc57a659fb4cd5406e4b925ca74e5
genlock <joshua9040@gmail.com>
2023-10-30 15:24:17 -0400

    v1.0.1

--- generators/shrek_generatorv1.py
+++ generators/shrek_generatorv1.py
@@ -11,13 +11,12 @@ logging.basicConfig(filename='shrek_prompts.log', level=logging.INFO)
 # Function to generate Shrek-related prompt
 def generate_shrek_prompt():
     prompts = [
-        "Shrek fighting a dragon on a volcano, radiant greatsword",
-        "Shrek dueling a medieval knight, dark fantasy, dark souls, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.3>",
-        "Shrek driving a futuristic motorcycle through the city, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.3>",
-        "Shrek evading an apocalyptic robot, cinematic, <lora:shrek:0.3>",
-        "Shrek and Donkey Kong on a magical quest, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.3>",
-        "Shrek as a king of an alien planet, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.3>",
-        "Shrek riding a futuristic motorcycle, tron, cyber, blue, virtual world, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.3>"
+        "Shrek fighting a dragon on a volcano, radiant greatsword, epic, dark fantasy, fire, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.2>",
+        "Shrek dueling a medieval knight, armor, sword, dark fantasy, dark souls, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.2>",
+        "Shrek driving a futuristic motorcycle through the city, cyberpunk, cyber, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.2>",
+        "Shrek evading an apocalyptic robot, detailed, 4k, high quality, clear, smooth, cinematic, intense, <lora:shrek:0.2>",
+        "Shrek as a king of an alien planet, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.2>",
+        "Shrek riding a futuristic motorcycle, tron, cyber, blue, virtual world, detailed, 4k, high quality, clear, smooth, cinematic, <lora:shrek:0.2>"
     ]
     chosen_prompt = random.choice(prompts)
     logging.info(f"{datetime.now()} - Chosen prompt: {chosen_prompt}")
@@ -29,16 +28,16 @@ api_url = "http://127.0.0.1:7860/sdapi/v1/txt2img"
 # Define the payload for animate_diff_args
 animate_diff_args = {
     "model": "mm_sd_v15_v2.ckpt",
-    "format": ["GIF","PNG"],
+    "format": ['GIF'],
     "enable": True,
     "video_length": 100,
-    "fps": 20,
+    "fps": 10,
     "loop_number": 0,
     "closed_loop": "R+P",
     "batch_size": 16,
     "stride": 1,
     "overlap": -1,
-    "interp": "Off",
+    "interp": "NO",
     "interp_x": 10,
     "latent_power": 1,
     "latent_scale": 32,
@@ -50,7 +49,7 @@ animate_diff_args = {
 # Define the JSON payload
 json_payload = {
     "prompt": generate_shrek_prompt(),
-    "negative_prompt": "bad quality, deformed, boring",
+    "negative_prompt": "bad quality, deformed, boring, mutation, amputation, missing appendage",
     "batch_size": 1,
     "sampler_name": "DPM++ 2M SDE Karras",
     "steps": 30,
@@ -71,7 +70,6 @@ headers = {
 response = requests.post(api_url, headers=headers, json=json_payload)
 
 # Debugging
-print("Full API Response:", response.json())
 print("HTTP Status Code:", response.status_code)
 
 # Parse the JSON response
