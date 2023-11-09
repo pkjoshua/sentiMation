7e2f4df956ddd0bb0858e7ed56163b7a9f1f8230
genlock <joshua9040@gmail.com>
2023-11-09 01:01:38 -0500

    v1.0.4

---
+++ generators/shrek_gen/call_of_shrek.py
@@ -0,0 +1,20 @@
+import subprocess
+
+# Call the first script
+result = subprocess.run(['python', 'shrek_CN_image_gen.py'], capture_output=True, text=True)
+
+# Check if the first script was successful
+if result.returncode == 0:
+    print("First script finished successfully.")
+    print("Output:", result.stdout)
+    # Call the second script
+    result2 = subprocess.run(['python', 'shrek_generator.py'], capture_output=True, text=True)
+    if result2.returncode == 0:
+        print("Second script finished successfully.")
+        print("Output:", result2.stdout)
+    else:
+        print("Second script failed.")
+        print("Error:", result2.stderr)
+else:
+    print("First script failed.")
+    print("Error:", result.stderr)
--- win/shrek_task_switch.ps1
+++ win/shrek_task_switch.ps1
@@ -1,28 +1,27 @@
 # Define the task name and task action
 $taskName = "Shrek_Generator"
-$taskAction1 = New-ScheduledTaskAction -Execute 'python' -Argument 'D:\sentiMation\generators\shrek_gen\shrek_CN_image_gen.py'
-$taskAction2 = New-ScheduledTaskAction -Execute 'python' -Argument 'D:\sentiMation\generators\shrek_gen\shrek_generatorv1.py'
+$taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-WindowStyle Hidden -c python D:\sentiMation\generators\shrek_gen\call_of_shrek.py' -WorkingDirectory 'D:\sentiMation\generators\shrek_gen'
 
 # Check if the task already exists
 $taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -eq $taskName}
 
 # If task doesn't exist, create it
-if ($taskExists -eq $null) {
+if (-not $taskExists) {
     Write-Host "Task does not exist. Creating new task."
-    $taskActions = @($taskAction1, $taskAction2)
-    $taskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Thursday -At 9am
-    Register-ScheduledTask -Action $taskActions -Trigger $taskTrigger -TaskName $taskName -User "NT AUTHORITY\SYSTEM" -Settings (New-ScheduledTaskSettingsSet -WakeToRun)
+    $taskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Friday -At 9am
+    $taskSettings = New-ScheduledTaskSettingsSet -WakeToRun -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+    $principal = New-ScheduledTaskPrincipal -UserId "PALADIN1\josh" -LogonType ServiceAccount -RunLevel Highest
+
+    Register-ScheduledTask -Action $taskAction -Principal $principal -Trigger $taskTrigger -TaskName $taskName -Settings $taskSettings
     Write-Host "Task created."
-}
-else {
+} else {
     # If task exists, toggle enable/disable
     Write-Host "Task already exists. Toggling state."
-    $currentState = (Get-ScheduledTask -TaskName $taskName).State
-    if ($currentState -eq "Disabled") {
+    $currentTask = Get-ScheduledTask -TaskName $taskName
+    if ($currentTask.State -eq "Disabled") {
         Enable-ScheduledTask -TaskName $taskName
         Write-Host "Task enabled."
-    }
-    else {
+    } else {
         Disable-ScheduledTask -TaskName $taskName
         Write-Host "Task disabled."
     }
