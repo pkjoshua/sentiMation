f33d76026fd743d30801c230b31266fe0fa05e38
genlock <joshua9040@gmail.com>
2023-11-30 19:39:07 -0500

    v1.2.13
    -- Went back to still image. Concern for hurting eyes with the psychedelic pattern plus the moving background

--- generators/pokemon/pokemon_background_overlay.py
+++ generators/pokemon/pokemon_background_overlay.py
@@ -23,35 +23,23 @@ def extract_frames(video_path, output_dir, file_prefix=""):
         count += 1
     return count
 
-# Function to overlay frames with a looping background
-def overlay_frames_with_background(frames_dir, background_video_path, mask_path, output_dir):
-    background_vidcap = cv2.VideoCapture(background_video_path)
-    mask = Image.open(mask_path).convert("L")  # Load the mask
-
-    bg_success, bg_image = background_vidcap.read()
-    bg_count = 0
+# Function to overlay frames with a static background image
+def overlay_frames_with_background(frames_dir, background_path, mask_path, output_dir):
+    background = Image.open(background_path).convert("RGBA")
+    mask = Image.open(mask_path).convert("L")
 
     frame_files = sorted(os.listdir(frames_dir), key=lambda x: int(x.split('_')[2].split('.png')[0]))
     for frame_file in frame_files:
         frame_path = os.path.join(frames_dir, frame_file)
         frame = Image.open(frame_path).convert("RGBA")
         
-        if not bg_success:
-            background_vidcap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            bg_success, bg_image = background_vidcap.read()
-            bg_count = 0
-
-        # Convert background frame to PIL Image and resize to match frame size
-        bg_image_pil = Image.fromarray(cv2.cvtColor(bg_image, cv2.COLOR_BGR2RGB)).convert("RGBA")
-        bg_resized = bg_image_pil.resize(frame.size)
-
+        # Resize background image to match frame size
+        bg_resized = background.resize(frame.size)
+        
         # Overlay frame on resized background using the mask
         combined = Image.composite(frame, bg_resized, mask)
         combined_path = os.path.join(output_dir, f"overlay_{frame_file}")
         combined.save(combined_path, format="PNG")
-        
-        bg_success, bg_image = background_vidcap.read()
-        bg_count += 1
 
 # Function to compile all frames into a video
 def create_video_from_frames(frames_dir, output_video_path, fps):
@@ -77,7 +65,7 @@ def create_video_from_frames(frames_dir, output_video_path, fps):
 
 # Paths and directories
 upscale_video_path = "upscale.mp4"
-background_video_path = "background.mp4"
+background_path = "background.png"
 pokemon_image_path = "current_pokemon_upscale.png"
 mask_path = "current_mask_upscale.png"  # Path to the mask image
 upscale_dir = "upscale_process"
@@ -95,7 +83,7 @@ for i in range(NUM_EXTRA_FRAMES):
     cv2.imwrite(os.path.join(upscale_dir, f"upscale_frame_{num_frames + i:04d}.png"), pokemon_img)
 
 # Overlay frames with background
-overlay_frames_with_background(upscale_dir, background_video_path, mask_path, output_dir)
+overlay_frames_with_background(upscale_dir, background_path, mask_path, output_dir)
 
 # Compile all overlay frames into a video
 create_video_from_frames(output_dir, "upscale_overlay.mp4", FPS)
